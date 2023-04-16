052ddce2df4db2896abffba03a6951e3df704f2e
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-04-16 08:31:09 +0200

    feat: Added example with values_list

--- sample_project/web/views.py
+++ sample_project/web/views.py
@@ -25,8 +25,14 @@ class CursorListView(CursorPaginateMixin, SampleListView):
 		return Random.objects.order_by('pk')
 
 
+class CursorValuesListView(CursorListView):
+	def get_queryset(self):
+		return super().get_queryset().values('pk', 'name')
+
+
 home_view = TemplateView.as_view(template_name='home.html')
 default_paginator_view = SampleListView.as_view(template_name='default.html')
 large_paginator_view = SampleListView.as_view(template_name='large.html')
 custom_template_paginator_view = SampleListView.as_view(template_name='custom_template.html')
 cursor_list_view = CursorListView.as_view(template_name='large.html')
+cursor_values_list_view = CursorValuesListView.as_view(template_name='large.html')
